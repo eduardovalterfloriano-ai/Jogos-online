3b92f2a054b4c094c105f451d40e049382174de4
eduardovalterfloriano-ai <eduardo.valter.floriano@escola.pr.gov.br>
2026-07-30 07:41:31 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função simples interativa ao clicar no botão do jogo
+function jogar(nomeDoJogo) {
+    alert(`Iniciando conexão com os servidores de "${nomeDoJogo}"... Prepare-se para jogar!`);
+}
